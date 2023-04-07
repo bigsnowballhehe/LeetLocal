0fa0a1a77ec98ba193b7382f4897ffa0c8bdbec2
Snlan <pridewui@foxmail.com>
2023-04-07 16:51:20 +0800

    feat:first commit

---
+++ 4.寻找两个正序数组的中位数.ts
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=4 lang=typescript
+ *
+ * [4] 寻找两个正序数组的中位数
+ */
+
+// @lc code=start
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+const tmp = [...nums1,...nums2]
+if(tmp.length ===1){
+    return tmp[0]
+}
+tmp.sort((a,b)=>a-b)
+if(tmp.length %2 ===0){
+    return (tmp[tmp.length/2 - 1] + tmp[tmp.length/2 ])/2
+}else{
+    return tmp[Math.round(tmp.length/2)-1]
+}
+};
+// @lc code=end
+
---
+++ 5.最长回文子串.ts
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=typescript
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+function longestPalindrome(s: string): string {
+  let left = 0
+  let right = 0
+  let maxLength = 0
+  const length = s.length
+  const getMaxStr = (str: string, i: number, j: number) => {
+    const length = str.length
+    while (i >= 0 && j < length && str[i] === str[j]) {
+      if (j - i + 1 > maxLength) {
+        left = i
+        right = j
+        maxLength = j-i+1
+      }
+      i--
+      j++
+    }
+  }
+  for (let index = 0; index < length; index++) {
+    getMaxStr(s, index, index)
+    getMaxStr(s,index,index+1)
+  }
+
+  return s.slice(left,right+1)
+}
+// @lc code=end
+
