8dff9f8a40e413efe8d54b20abeadaa2e0dac251
Snlan <pridewui@foxmail.com>
2023-05-25 11:30:01 +0800

    feat: add chore

--- VueCoreDesign/Reactive/proxy.js
+++ VueCoreDesign/Reactive/proxy.js
@@ -6,87 +6,146 @@ const p = Promise.resolve()
 
 let isFlushing = false
 function flushJob() {
-    if(isFlushing){
-        return
-    }
-    isFlushing = true
-    p.then(() => {
-        jobQueue.forEach(job=>job())
-    }).finally(() => {
-        isFlushing = false 
-    })
+  if (isFlushing) {
+    return
+  }
+  isFlushing = true
+  p.then(() => {
+    jobQueue.forEach((job) => job())
+  }).finally(() => {
+    isFlushing = false
+  })
 }
 function cleanup(effectFn) {
-    for (let i = 0; i < effectFn.deps.length; i++) {
-        const deps = effectFn.deps[i];
-        deps.delete(effectFn)
-    }
+  for (let i = 0; i < effectFn.deps.length; i++) {
+    const deps = effectFn.deps[i]
+    deps.delete(effectFn)
+  }
 }
 
-
-function effect(fn,options={}) {
-    const effectFn = () => {
-        cleanup(effectFn)
-        activeEffect = effectFn
-        effectStack.push(effectFn)
-        fn()
-        effectStack.pop()
-        activeEffect = effectStack[effectStack.length-1]
-    }
-    effectFn.options = options
-    effectFn.deps = []
+function effect(fn, options = {}) {
+  const effectFn = () => {
+    cleanup(effectFn)
+    activeEffect = effectFn
+    effectStack.push(effectFn)
+    const res = fn()
+    effectStack.pop()
+    activeEffect = effectStack[effectStack.length - 1]
+    return res
+  }
+  effectFn.options = options
+  effectFn.deps = []
+  if (!options.lazy) {
     effectFn()
+  }
+  return effectFn
 }
 
+function computed(getter) {
+  let value
+  let dirty = true
+  const effectFn = effect(getter, {
+    lazy: true,
+    scheduler() {
+      dirty = true
+      trigger(obj, "value")
+    },
+  })
+  const obj = {
+    get value() {
+      if (dirty) {
+        value = effectFn()
+        dirty = false
+      }
+      track(obj, "value")
+      return value
+    },
+  }
+  return obj
+}
+
+function watch(source, cb) {
+  let getter
+  if (typeof source === "function") {
+    getter = source
+  } else {
+    getter = () => traverse(source)
+  }
+  let odlValue, newValue
+  const job = () => {
+    newValue = effectFn()
+    cb(newValue, odlValue)
+    odlValue = newValue
+  }
+  const effectFn = effect(() => getter(), {
+    scheduler: job,
+    lazy: true,
+  })
+  if (options.immediate) {
+    job()
+  } else {
+    odlValue = effectFn()
+  }
+}
+
+function traverse(value, seen = new Set()) {
+  if (typeof value !== "object" || value === null || seen.has(value)) {
+    return
+  }
+  seen.add(value)
+  for (const k in value) {
+    traverse(value[k], seen)
+  }
+  return value
+}
 const bucket = new WeakMap()
 
-const data = {text:'hello world'}
+const data = { text: "hello world" }
 
-function track(target,key) {
-    if(!activeEffect){
-        return target[key]
-    }
-    let depsMap = bucket.get(target)
-    if(!depsMap){
-        bucket.set(target,(depsMap = new Map()))
-    }
-    let deps = depsMap.get(key)
-    if(!deps){
-        depsMap.set(key,(deps = new Set()))
-    }
-    deps.add(activeEffect)
-    activeEffect.deps.push(deps)
+function track(target, key) {
+  if (!activeEffect) {
+    return target[key]
+  }
+  let depsMap = bucket.get(target)
+  if (!depsMap) {
+    bucket.set(target, (depsMap = new Map()))
+  }
+  let deps = depsMap.get(key)
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()))
+  }
+  deps.add(activeEffect)
+  activeEffect.deps.push(deps)
 }
-function trigger(target,key) {
-    const depsMap = bucket.get(target)
-    if(!depsMap){
-        return
+function trigger(target, key) {
+  const depsMap = bucket.get(target)
+  if (!depsMap) {
+    return
+  }
+  const effects = depsMap.get(key)
+  const effectsToRun = new Set()
+  effects &&
+    effects.forEach((effectFn) => {
+      if (effectFn !== activeEffect) {
+        effectsToRun.add(effectFn)
+      }
+    })
+  effectsToRun.forEach((effectFn) => {
+    if (effectFn.options.scheduler) {
+      effectFn.options.scheduler(effectFn)
+    } else {
+      effectFn()
     }
-    const effects = depsMap.get(key)
-    const effectsToRun = new Set()
-    effects && effects.forEach((effectFn) => {
-        if(effectFn !== activeEffect){
-            effectsToRun.add(effectFn)
-        }
-    })
-    effectsToRun.forEach(effectFn => {
-        if(effectFn.options.scheduler){
-            effectFn.options.scheduler(effectFn)
-        }else{
-            effectFn()
-        }
-        
-    })
-    //effects && effects.forEach(fn=>fn())
+  })
+  //effects && effects.forEach(fn=>fn())
 }
-const obj = new Proxy(data,{
-    get(target,key){
-        track(target,key)
-        return target[key]
-    },
-    set(target,key,newVal){
-        target[key] = newVal
-       trigger(target,key)
-      
-    }
+const obj = new Proxy(data, {
+  get(target, key) {
+    track(target, key)
+    return target[key]
+  },
+  set(target, key, newVal) {
+    target[key] = newVal
+    trigger(target, key)
+  },
 })
